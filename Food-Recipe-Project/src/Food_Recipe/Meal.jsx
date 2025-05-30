7418dd2a2a4718fe836c4af08d945e67672158ec
Shakhoyat <shakoyatsujon@gmail.com>
2025-05-30 11:58:05 +0600

    add Bootstrap styling to Meal component and main entry point

--- Food-Recipe-Project/src/Food_Recipe/Meal.jsx
+++ Food-Recipe-Project/src/Food_Recipe/Meal.jsx
@@ -20,21 +20,35 @@ const Meal = () => {
     }, []);
 
     return (
-        <div>
-            <h1>Meal Data</h1>
-            {mealData.length > 0 ? (
-                <ul style={{ display: 'flex', flexWrap: 'wrap', gap: '20px', listStyleType: 'none', padding: 0 }}>
-                    {mealData.map((meal) => (
-                        <li key={meal.idMeal}>
-                            <img src={meal.strMealThumb} alt={meal.strMeal} style={{ width: '220px', borderRadius: '10px', border: '2px solid blue' }} />
-                            <p>{meal.strMeal}</p>
-                        </li>
-                    ))}
-                </ul>
-            ) : (
-                <p>Loading...</p>
-            )}
-        </div>
+        <>
+            <div className="mx-auto text-center my-3">
+                <button type="button" class="btn btn-primary">Primary</button>
+                <button type="button" class="btn btn-secondary">Secondary</button>
+                <button type="button" class="btn btn-success">Success</button>
+                <button type="button" class="btn btn-danger">Danger</button>
+                <button type="button" class="btn btn-warning">Warning</button>
+                <button type="button" class="btn btn-info">Info</button>
+                <button type="button" class="btn btn-light">Light</button>
+                <button type="button" class="btn btn-dark">Dark</button>
+
+                <button type="button" class="btn btn-link">Link</button>
+            </div >
+            <div>
+
+                {mealData.length > 0 ? (
+                    <ul style={{ display: 'flex', flexWrap: 'wrap', gap: '20px', listStyleType: 'none', padding: 0 }}>
+                        {mealData.map((meal) => (
+                            <li key={meal.idMeal}>
+                                <img src={meal.strMealThumb} alt={meal.strMeal} style={{ width: '220px', borderRadius: '10px', border: '2px solid blue' }} />
+                                <p>{meal.strMeal}</p>
+                            </li>
+                        ))}
+                    </ul>
+                ) : (
+                    <p>Loading...</p>
+                )}
+            </div>
+        </>
     )
 }
 
